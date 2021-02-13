8365f7f89e94ae098a37d684a4a3547adaac996c
Cristopher Ortega <ortega.cpp@gmail.com>
2021-02-13 02:12:33 -0400

    xDai & Matic configuration

--- src/index.tsx
+++ src/index.tsx
@@ -12,14 +12,14 @@ function App() {
         showModal={showModal}
         onClose={() => setShowModal(false)}
         onReady={(params) => console.log("MODAL IS READY =======>", params)}
-        withdrawalAddress={"0x75e4DD0587663Fce5B2D9aF7fbED3AC54342d3dB"}
-        routerPublicIdentifier="vector7tbbTxQp8ppEQUgPsbGiTrVdapLdU5dH7zTbVuXRf1M4CEBU9Q"
-        depositAssetId={"0xbd69fC70FA1c3AED524Bb4E82Adc5fcCFFcD79Fa"}
-        depositChainId={5}
-        depositChainProvider="https://goerli.infura.io/v3/"
-        withdrawAssetId={"0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1"}
-        withdrawChainId={80001}
-        withdrawChainProvider="https://rpc-mumbai.matic.today"
+        withdrawalAddress={"0x70C58b28F5E39da89bEe0e6E8623e3faF51f0Ed1"}
+        routerPublicIdentifier="vector8AXWmo3dFpK1drnjeWPyi9KTy9Fy3SkCydWx8waQrxhnW4KPmR"
+        depositAssetId={"0x71850b7E9Ee3f13Ab46d67167341E4bDc905Eef9"}
+        depositChainId={100}
+        depositChainProvider="https://rpc.xdaichain.com/​"
+        withdrawAssetId={"0xb371248Dd0f9E4061ccf8850E9223Ca48Aa7CA4b"}
+        withdrawChainId={137}
+        withdrawChainProvider="https://rpc-mainnet.maticvigil.com/"
       />
     </>
   );
